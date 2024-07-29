61d9fe92eaf63a84980ce740d54934bf9f1917e0
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-29 13:45:47 +0200

    fix: added base routing
    /spend 15m

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import { Bases } from "./src/entities/base/base";
+
+const base = `scheme-viewer`;
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
 	site: `https://nico-i.github.io`,
-	base: `scheme-viewer`,
+	base,
+	trailingSlash: `never`,
+	redirects: {
+		"/": `${base}/${Bases.BASE16}`,
+	},
 });
