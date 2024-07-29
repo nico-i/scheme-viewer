d5eb4405d32134c5cd96e49cd2a8e7c708d76900
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-29 13:05:32 +0200

    feat: added gh pages config
    /spend 10m

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
+	site: `https://nico-i.github.io`,
+	base: `scheme-viewer`,
 });
